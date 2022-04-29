7a8cb357f5559d8ccb6821ee310761e2940431bd
tcqgg2018 <66042761+tcqgg2018@users.noreply.github.com>
2022-04-29 18:56:52 +0800

    Update Countdown.js

--- Countdown.js
+++ Countdown.js
@@ -107,9 +107,9 @@ function icon_now(num){
 $done({
 title:title_random(tnumcount(Number(nowlist))),
 icon:icon_now(tnumcount(Number(nowlist))),
+'icon-color':'#00BC12',
 content:tlist[nowlist][0]+":"+today(tnumcount(nowlist))+"天,"+tlist[Number(nowlist) + Number(1)][0] +":"+ tnumcount(Number(nowlist) + Number(1))+ "天,"+tlist[Number(nowlist) + Number(2)][0]+":"+tnumcount(Number(nowlist) + Number(2))+"天"
 })
-"icon-color":"#00BC12",
 function title_random(num){
   let r = Math.floor((Math.random()*12)+1);
   let dic = {
