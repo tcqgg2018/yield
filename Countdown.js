e47bba3c596f5242988323de2fcfc5844169cd9a
tcqgg2018 <66042761+tcqgg2018@users.noreply.github.com>
2022-04-29 17:09:05 +0800

    Update Countdown.js

--- Countdown.js
+++ Countdown.js
@@ -107,14 +107,16 @@ function icon_now(num){
 $done({
 title:title_random(tnumcount(Number(nowlist))),
 icon:icon_now(tnumcount(Number(nowlist))),
-if(icon_now=="hare"){
-  return 'icon-color':'#FCEFE8'
-}else if(icon_now=="timer"){
-  return 'icon-color':'#F2BE45'
-}else if(icon_now=="gift"){
-  return 'icon-color':'#FF2121'
-}else{
-  return 'icon-color':'#00BC12'
+function icon_color(icon_now){
+  if(icon_now=="hare"){
+    return 'icon-color':'#FCEFE8'
+  }else if(icon_now=="timer"){
+    return 'icon-color':'#F2BE45'
+  }else if(icon_now=="gift"){
+    return 'icon-color':'#FF2121'
+  }else{
+    return 'icon-color':'#00BC12'
+  }
 }
 content:tlist[nowlist][0]+":"+today(tnumcount(nowlist))+"天,"+tlist[Number(nowlist) + Number(1)][0] +":"+ tnumcount(Number(nowlist) + Number(1))+ "天,"+tlist[Number(nowlist) + Number(2)][0]+":"+tnumcount(Number(nowlist) + Number(2))+"天"
 })
