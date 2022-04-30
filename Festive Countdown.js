0f27a4a6474c75f8bfd03bb0323b71b04cae67bb
tcqgg2018 <66042761+tcqgg2018@users.noreply.github.com>
2022-04-30 08:53:36 +0800

    Update Festive Countdown.js

--- Festive Countdown.js
+++ Festive Countdown.js
@@ -91,7 +91,7 @@ function datenotice() {
 }
 $done({
 title:"节假日倒计时",
-icon:"list.dash.header.rectangle",
-'icon-color': "#5AC8FA",
+icon:"tortoise",
+'icon-color': "#16A951",
 content:tlist[nowlist][0]+"  :  "+today(tnumcount(nowlist))+"天\n"+tlist[Number(nowlist) + Number(1)][0] +"  :  "+ tnumcount(Number(nowlist) + Number(1))+ "天\n"+tlist[Number(nowlist) + Number(2)][0]+"  :  "+tnumcount(Number(nowlist) + Number(2))+"天"
 })
